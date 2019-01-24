83810fafec5c19e83c4e902eec38df75d1068714
Prashanth N <prashanth.t.r.n@gmail.com>
2019-01-24 23:55:20 +0530

    user check auth and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  include ActionController::Serialization
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  include GlobalExceptionHandler
+
+  private
+
+  def authenticate_request
+    auth_header = request.headers['Authorization'].presence
+    if auth_header
+      auth_token = auth_header.split(' ').last
+      @current_user = User.find_by(auth_token: auth_token)
+    end
+    access_denied unless current_user
+  end
+
+  def access_denied(message = 'Access denied')
+    error = { error: message }
+    render json: error, status: :unauthorized
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  apipie
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :taskboard do
+    scope :api, defaults: { formats: :json } do
+      namespace :v1 do
+        post '/login', to: 'users#login'
+        post '/signup', to: 'users#create'
+        resources :users, only: [:index]
+        resources :lists do
+          get 'list_members', on: :member
+          post 'assign_members', on: :member
+          post 'unassign_members', on: :member
+          resources :cards
+        end
+        resources :comments
+      end
+    end
+  end
 end
