1dd4878c893e66e745470aacb380fb92530767d5
Prashanth N <prashanth.t.r.n@gmail.com>
2019-01-25 17:36:03 +0530

    redirect root to apipie

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   apipie
+  get "/" => redirect("/apipie")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :taskboard do
     scope :api, defaults: { formats: :json } do
